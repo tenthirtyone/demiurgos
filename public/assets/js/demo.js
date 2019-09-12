6a6324690ce22de26a828ec949479cc1d715fa3e
tenthirtyone <alexsherbuck@gmail.com>
2019-09-12 11:00:43 -0700

    email signup

--- public/assets/js/demo.js
+++ public/assets/js/demo.js
@@ -24,3 +24,24 @@ $(".About-Header").click(function() {
     1000
   );
 });
+
+$(".btn-sign-up").click(function() {
+  var email = $(".email").val();
+
+  if (validateEmail(email)) {
+    $.ajax({
+      type: "POST",
+      url: "/signup",
+      data: { email },
+      success: res => {
+        console.log(res);
+      },
+      dataType: "json"
+    });
+  }
+});
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
