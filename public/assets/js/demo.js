19c6fe8ca6d8225a828e7844a5d2877cbecd7cbe
tenthirtyone <alexsherbuck@gmail.com>
2019-09-26 08:20:07 -0700

    create cards

--- public/assets/js/demo.js
+++ public/assets/js/demo.js
@@ -1,4 +1,4 @@
-$(".Home-Header").click(function() {
+$(".Home-Header").click(function () {
   $([document.documentElement, document.body]).animate(
     {
       scrollTop: $("#Home").offset().top
@@ -7,7 +7,7 @@ $(".Home-Header").click(function() {
   );
 });
 
-$(".Feature-Header").click(function() {
+$(".Feature-Header").click(function () {
   $([document.documentElement, document.body]).animate(
     {
       scrollTop: $("#Features").offset().top
@@ -16,7 +16,7 @@ $(".Feature-Header").click(function() {
   );
 });
 
-$(".About-Header").click(function() {
+$(".About-Header").click(function () {
   $([document.documentElement, document.body]).animate(
     {
       scrollTop: $("#About").offset().top
@@ -25,7 +25,7 @@ $(".About-Header").click(function() {
   );
 });
 
-$(".btn-sign-up").click(function() {
+$(".btn-sign-up").click(function () {
   var email = $(".email").val();
 
   if (validateEmail(email)) {
@@ -54,3 +54,40 @@ function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+
+
+$('.card1').click(function () {
+  fetch("https://credibleassets.com/api/card", {
+    method: "POST",
+    body: JSON.stringify({ card: "1" }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  });
+  alert('After selecting a card, take the user to your payment processor. Once the user completes the transaction, send a request to credible\'s API to create the asset with the (optional) user id. \n \n To view the card and transaction visit the Credible dashboard.');
+})
+
+$('.card2').click(function () {
+  fetch("https://credibleassets.com/api/card", {
+    method: "POST",
+    body: JSON.stringify({ card: "2" }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  });
+  alert('After selecting a card, take the user to your payment processor. Once the user completes the transaction, send a request to credible\'s API to create the asset with the (optional) user id. \n \n To view the card and transaction visit the Credible dashboard.');
+})
+
+$('.card3').click(function () {
+  fetch("https://credibleassets.com/api/card", {
+    method: "POST",
+    body: JSON.stringify({ card: "3" }),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  });
+  alert('After selecting a card, take the user to your payment processor. Once the user completes the transaction, send a request to credible\'s API to create the asset with the (optional) user id. \n \n To view the card and transaction visit the Credible dashboard.');
+})
